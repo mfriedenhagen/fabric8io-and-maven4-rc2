aeb22fbeb41b20da74fa17830d4a8e05d50032a8
Mirko Friedenhagen <mfriedenhagen@gmx.de>
2024-12-18 17:57:57 +0100

    init

---
+++ src/test/java/com/example/oss_demo/OssDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oss_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
